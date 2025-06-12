951a061c36783ebc07f9b63013b89a649eae6bbe
Jackson Bambrick <jacksonbambrick253@gmail.com>
2025-06-12 15:35:53 -0700

    Create coming_soon.py
    Creation of coming soon page

---
+++ pages/coming_soon.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import os
+
+def show_sql_project():
+    st.title("Coming Soon: SQL Project")
+
+    st.write(
+        """
+        Project description here
+    )
+
+    st.subheader("Anticipated Technologies:")
+    st.write(
+        """
+        SQL, Tableau, Github, add others throughout
+        """
+    )
+
+    st.info("Stay tuned for updates! I'll be sharing progress and details on this page as the project develops.")

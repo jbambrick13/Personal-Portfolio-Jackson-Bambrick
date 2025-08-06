75dc9e310ce24644e6153b6c4af481df89ce7f7c
Jackson Bambrick <jacksonbambrick253@gmail.com>
2025-08-06 13:32:33 -0700

    test: page selection logic

--- Welcome_To_My_Portfolio.py
+++ Welcome_To_My_Portfolio.py
@@ -15,30 +15,31 @@ st.set_page_config(
     layout="wide",
 )
 
-# Page Selection 
-#st.sidebar.title("Select Page")
-#page_selection = st.sidebar.selectbox(
-#    "Go to",
-#    [
-#        "About Me",
-#        "Resume",
-#        "Project Central",
-#        "Lacrosse Championship Prediction & Visualization Project",
-#        "Automated Weather Data Pipeline and Dashboard"
-#    ]
-#)
+# Page Selection
+st.sidebar.title("Select Page")
+page_selection = st.sidebar.selectbox(
+    "Go to",
+    [
+        "About Me",
+        "Resume",
+        "Project Central",
+        "Lacrosse Championship Prediction & Visualization Project",
+        "Automated Weather Data Pipeline and Dashboard"
+        "Coming Soon: ML and the NBA G.O.A.T. Project"
+    ]
+)
 
 # Display Selected Page
-#if page_selection == "About Me":
-#    about_me.show_about_me()
-#elif page_selection == "Resume":
-#    resume.show_resume()
-#elif page_selection == "Project Central":
-#    project_central.show_project_central()
-#elif page_selection == "Lacrosse Championship Prediction & Visualization Project":
-#    lacrosse_prediction_dashboard.show_lacrosse_prediction_dashboard()
-#elif page_selection == "Automated Weather Data Pipeline and Dashboard":
-#    weather_project.show_weather_project()
-#elif page_selection == "Coming Soon: ML and the NBA G.O.A.T. Project":
-#    coming_soon.show_coming_soon()
+if page_selection == "About Me":
+    about_me.show_about_me()
+elif page_selection == "Resume":
+    resume.show_resume()
+elif page_selection == "Project Central":
+    project_central.show_project_central()
+elif page_selection == "Lacrosse Championship Prediction & Visualization Project":
+    lacrosse_prediction_dashboard.show_lacrosse_prediction_dashboard()
+elif page_selection == "Automated Weather Data Pipeline and Dashboard":
+    weather_project.show_weather_project()
+elif page_selection == "Coming Soon: ML and the NBA G.O.A.T. Project":
+    coming_soon.show_coming_soon()
 

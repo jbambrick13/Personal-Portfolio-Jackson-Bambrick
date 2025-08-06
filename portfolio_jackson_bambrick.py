9ade734cdc64708f77ab81fa4014bc62408a4904
Jackson Bambrick <jacksonbambrick253@gmail.com>
2025-08-06 13:23:26 -0700

    fix: update page selection logic

--- portfolio_jackson_bambrick.py
+++ portfolio_jackson_bambrick.py
@@ -7,14 +7,15 @@ from pages import resume
 from pages import project_central
 from pages import lacrosse_prediction_dashboard
 from pages import weather_project
+from pages import coming_soon
 
-# --- Page Configuration ---
+# Page Configuration
 st.set_page_config(
     page_title="Jackson Bambrick - Personal Portfolio",
     layout="wide",
 )
 
-# --- Page Selection ---
+# Page Selection
 st.sidebar.title("Select Page")
 page_selection = st.sidebar.selectbox(
     "Go to",
@@ -27,7 +28,7 @@ page_selection = st.sidebar.selectbox(
     ]
 )
 
-# --- Display Selected Page ---
+# Display Selected Page
 if page_selection == "About Me":
     about_me.show_about_me()
 elif page_selection == "Resume":
@@ -38,3 +39,5 @@ elif page_selection == "Lacrosse Championship Prediction & Visualization Project
     lacrosse_prediction_dashboard.show_lacrosse_prediction_dashboard()
 elif page_selection == "Automated Weather Data Pipeline and Dashboard":
     weather_project.show_weather_project()
+elif page_selection == "Coming Soon: ML and the NBA G.O.A.T. Project":
+    coming_soon.show_coming_soon()

6e7e4768e69a011e1ca7993c88291b4c52db910f
Jackson Bambrick <jacksonbambrick253@gmail.com>
2025-08-06 13:20:35 -0700

    feat: creation of coming soon page

--- pages/coming_soon.py
+++ pages/coming_soon.py
@@ -1,22 +1,13 @@
-# Import necessary packages
+# Import needed packes
 import streamlit as st
 
 def show_coming_soon():
-    st.title("Coming Soon: SQL Project")
+    st.title("Coming Soon: ML and the NBA G.O.A.T. Project")
 
     st.write(
         """
-        This project is currently in development and will focus on advanced SQL techniques, database design, and data manipulation. 
-        It will involve using an API to collect publicly available satellite data and using SQL to allow for complex queries, while 
-        also taking advantadge of Tableu's visualization capacilites to tell data stories
+        This project is still in the development phase. Please reach out or check back for updates!
         """
     )
 
-    st.subheader("Anticipated Technologies:")
-    st.write(
-        """
-        SQL, Tableau, Github, and possibly more to come!
-        """
-    )
-
-    st.info("Stay tuned for updates! I'll be sharing progress and details on this page as the project develops.")
+    st.markdown("---")
